426026746538258898753ab44e58617787388f4e
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-13 12:14:24 +0200

    Added MongoDB database and updated the get method

--- src/main/java/com/aljoschazoeller/java/hhjava242_404_springdata/Character.java
+++ src/main/java/com/aljoschazoeller/java/hhjava242_404_springdata/Character.java
@@ -1,5 +1,8 @@
 package com.aljoschazoeller.java.hhjava242_404_springdata;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("characters")
 public record Character (
         String id,
         String name,
