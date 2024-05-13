d3674125083c4e62a3f906730bc5449e1422dd54
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-13 11:23:11 +0200

    Initial commit

---
+++ src/main/java/com/aljoschazoeller/java/hhjava242_404_springdata/HhJava242404SpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.java.hhjava242_404_springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava242404SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava242404SpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aljoschazoeller/java/hhjava242_404_springdata/HhJava242404SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.java.hhjava242_404_springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava242404SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
