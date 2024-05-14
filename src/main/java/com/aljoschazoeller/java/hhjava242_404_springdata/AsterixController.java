5cd4f5948346953e1209f4abf60c1ced265dd8e5
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-14 10:27:43 +0200

    Add swagger annotations

--- src/main/java/com/aljoschazoeller/java/hhjava242_404_springdata/AsterixController.java
+++ src/main/java/com/aljoschazoeller/java/hhjava242_404_springdata/AsterixController.java
@@ -2,6 +2,10 @@ package com.aljoschazoeller.java.hhjava242_404_springdata;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,19 +37,28 @@ public class AsterixController {
         return asterixService.addCharacter(newCharacter);
     }
 
-    @Operation(summary = "Get a single character")
+    @Operation(summary = "Get a single character", parameters = {@Parameter(name = "id", description = "Character id", example = "66424303b4ef3d7579e188cb")})
     @GetMapping("{id}")
     Character getCharacter(@PathVariable String id) {
         return asterixService.getCharacterById(id);
     }
 
-    @Operation(summary = "Delete a single character")
+    @Operation(summary = "Delete a single character", parameters = {@Parameter(name = "id", description = "Character id", example = "66424303b4ef3d7579e188cb")})
     @DeleteMapping("{id}")
     void deleteCharacter(@PathVariable String id) {
         asterixService.deleteCharacterById(id);
     }
 
-    @Operation(summary = "Update a single character")
+    @Operation(
+            summary = "Update a single character",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "Character id",
+                            example = "66424303b4ef3d7579e188cb"
+                    )
+            }
+    )
     @PutMapping("{id}")
     Character updateCharacter(@PathVariable String id, @RequestBody NewCharacter newCharacter) {
         if (asterixService.exitsCharacterById(id)) {
